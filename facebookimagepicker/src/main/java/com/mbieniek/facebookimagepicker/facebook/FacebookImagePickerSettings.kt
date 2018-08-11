81224268e8dbba5448be93c72b627fb8f3e57044
fpareja-lecaros <fpareja-lecaros@art.com>
2018-08-10 17:04:42 -0700

    Accidentally made debugging a constant

--- facebookimagepicker/src/main/java/com/mbieniek/facebookimagepicker/facebook/FacebookImagePickerSettings.kt
+++ facebookimagepicker/src/main/java/com/mbieniek/facebookimagepicker/facebook/FacebookImagePickerSettings.kt
@@ -10,7 +10,7 @@ object FacebookImagePickerSettings {
     var imagesSelectedText : String = "Select (%d)"
     var maximumImagesSelectedText : String = "You\'ve reached the maximum limit of photos that can be selected!"
     var placeholderDrawableColor : String = "#000000"
-    val debugging : Boolean = false
+    var debugging : Boolean = false
 
     val picassoAvailable = picassoCheck()
     val glideAvailable = glideCheck()
